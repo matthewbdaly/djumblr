1aa20090c6f4d264c42b24a01c7a26468e3e1278
Matthew Daly <matthewbdaly@gmail.com>
2018-01-25 17:45:39 +0000

    Created post types

--- tumblelog/admin.py
+++ tumblelog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.TextPost)
+admin.site.register(models.ImagePost)
+admin.site.register(models.LinkPost)
--- tumblelog/models.py
+++ tumblelog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class BasePost(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    slug = models.SlugField(max_length=40, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class TextPost(BasePost):
+    text = models.TextField()
+
+
+class ImagePost(BasePost):
+    image = models.ImageField()
+
+
+class LinkPost(BasePost):
+    link = models.URLField()
